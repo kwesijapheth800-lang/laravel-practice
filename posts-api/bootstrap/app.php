304356e57bbae71e0fc892c1e6059431e3c0b3eb
Japheth Arthur <kwesijapheth800@gmail.com>
2026-04-07 20:32:52 +0000

    Fixed a bug

--- posts-api/bootstrap/app.php
+++ posts-api/bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(fn() => response()->json(['message' => 'Unauthenticated.'], 401));
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
